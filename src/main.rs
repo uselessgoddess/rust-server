900ca980e8b30395ae31dd12d6a66f94f2a72eee
uselessgoddess <dodickgod@gmail.com>
2022-01-09 15:45:28 +0300

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,46 @@
+#![feature(entry_insert)]
+
+mod model;
+
+use actix_web::web::Data;
+use actix_web::{guard, web, App, HttpResponse, HttpServer, Result};
+use async_graphql::http::{playground_source, GraphQLPlaygroundConfig};
+use async_graphql::{EmptyMutation, EmptySubscription};
+use async_graphql_actix_web::{GraphQLRequest, GraphQLResponse};
+use async_std::sync::{Arc, RwLock};
+use std::collections::HashMap;
+
+type Schema = async_graphql::Schema<model::Query, model::Mutation, EmptySubscription>;
+
+async fn index(schema: web::Data<Schema>, req: GraphQLRequest) -> GraphQLResponse {
+    schema.execute(req.into_inner()).await.into()
+}
+
+async fn index_playground() -> Result<HttpResponse> {
+    let source = playground_source(GraphQLPlaygroundConfig::new("/").subscription_endpoint("/"));
+    Ok(HttpResponse::Ok()
+        .content_type("text/html; charset=utf-8")
+        .body(source))
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let db = Arc::new(RwLock::new(HashMap::new()));
+    let schema = Schema::new(
+        model::Query::new(db.clone()),
+        model::Mutation::new(db.clone()),
+        EmptySubscription,
+    );
+
+    println!("Playground: http://localhost:8000");
+
+    HttpServer::new(move || {
+        App::new()
+            .app_data(Data::new(schema.clone()))
+            .service(web::resource("/").guard(guard::Post()).to(index))
+            .service(web::resource("/").guard(guard::Get()).to(index_playground))
+    })
+    .bind("127.0.0.1:8000")?
+    .run()
+    .await
+}
